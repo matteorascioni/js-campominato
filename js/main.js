62eb04bc6fda1b27b337d81617cee7d9f92643d9
Matteo <Rascioni>
2020-10-23 18:28:52 +0200

    100% senza bonus

--- js/main.js
+++ js/main.js
@@ -22,11 +22,25 @@ for (var i = 0; i < numInseriti; i++) {
     var utente = parseInt(prompt('Inserisci un numero') );
 
     while (numUtente.includes(utente) || isNaN(utente) ) {
-        utente = parseInt(prompt('Numero già inserito inserisci un altro valore') );
-        
-    }
-    console.log(numUtente)
 
+        utente = parseInt(prompt('Numero già inserito inserisci un altro valore') );
+    }
+
+    numUtente.push(utente);
+
+    if(! listaDoppioniPc.includes(utente)) {
+        console.log('numero corretto prosegui');
+    }   else {
+        console.log('numero già inserito');
+        alert('hai perso');
+        break;
+    }
+
+    console.log(numUtente)
+}
+
+if( numInseriti == numUtente.length) {
+    console.log('Hai vinto');
 }
 
 
