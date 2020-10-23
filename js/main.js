336cf9b2999f69e861d190cf2b5b48a4a1c412fa
Matteo <Rascioni>
2020-10-23 17:55:18 +0200

    50%

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // GENERAZIONE NUMERI CASUALI PC SENZA NUMERI DOPPI
 var listaDoppioniPc = [];
-
 var size = 16;
 
 while (listaDoppioniPc.length < size) {
@@ -14,11 +13,23 @@ while (listaDoppioniPc.length < size) {
 console.log(listaDoppioniPc);
 
 // RICHIEDI ALL'UTENTE DOVE VUOI FAR INSERIRE I NUMERI
+var numUtente = [];
+var numInseriti = 5;
 
-for (var i = 0; i < 86; i++) {
-    var numUtente = parseInt(prompt('Inserisci un numero') );
-    
+
+for (var i = 0; i < numInseriti; i++) {
+
+    var utente = parseInt(prompt('Inserisci un numero') );
+
+    while (numUtente.includes(utente) || isNaN(utente) ) {
+        utente = parseInt(prompt('Numero già inserito inserisci un altro valore') );
+        
+    }
+    console.log(numUtente)
+
 }
+
+
 
 
 
