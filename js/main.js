b5e5fd711931ef449a4d3364865508039e3f2c9d
Matteo <Rascioni>
2020-10-23 15:52:28 +0200

    primo push

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// GENERAZIONE NUMERI CASUALI PC SENZA NUMERI DOPPI
+var listaDoppioniPc = [];
+
+var size = 16;
+
+while (listaDoppioniPc.length > size) {
+    var number = getRandomNumber(1, 100) 
+
+    if (! listaDoppioniPc.includes(number)) {
+        listaDoppioniPc.push(number);
+    }
+}
+
+console.log(listaDoppioniPc);
+
+
+
+
+
+// FUNZIONI 
+
+// FUNZIONE PER GENERARE NUMERI CASUALI
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
